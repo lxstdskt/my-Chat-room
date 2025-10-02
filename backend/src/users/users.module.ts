2585bfc03c3e58a3b70f9f49565457fbb87cb1b8
lxstdskt <1931570677@qq.com>
2025-10-02 13:14:31 +0800

    解决users模块冲突

---
+++ backend/src/users/users.module.ts
@@ -0,0 +1,16 @@
+// import { Module } from '@nestjs/common';
+// import { UsersService } from './users.service';
+// import { UsersService } from './users.service';
+
+// @Module({
+//   providers: [UsersService]
+// })
+// export class UsersModule {}
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Module({
+  providers: [UsersService],
+  exports: [UsersService],
+})
+export class UsersModule {}
